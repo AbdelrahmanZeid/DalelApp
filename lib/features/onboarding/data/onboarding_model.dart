29670556e5fe9b675c88019beb2fdb69f98f9520
Abdelrahman <zeid4643@gmail.com>
2023-11-10 23:09:27 +0200

    finish splash view

--- lib/features/onboarding/data/onboarding_model.dart
+++ lib/features/onboarding/data/onboarding_model.dart
@@ -1,3 +1,6 @@
+import 'package:dalel/core/utils/app_assets.dart';
+import 'package:dalel/core/utils/app_strings.dart';
+
 class OnBoardingModel {
   final String image;
   final String title;
@@ -8,3 +11,18 @@ class OnBoardingModel {
     required this.subTitle,
   });
 }
+
+List<OnBoardingModel> onBoardingListItem = [
+  OnBoardingModel(
+      image: Assets.assetsImagesOnBoardingOne,
+      title: AppStrings.onboardingItemTitleOne,
+      subTitle: AppStrings.onboardingItemSubTitleOne),
+  OnBoardingModel(
+      image: Assets.assetsImagesOnBoardingTwo,
+      title: AppStrings.onboardingItemSubTitleTwo,
+      subTitle: AppStrings.onboardingItemSubTitleTwo),
+  OnBoardingModel(
+      image: Assets.assetsImagesOnBoardingThree,
+      title: AppStrings.onboardingItemTitleThree,
+      subTitle: AppStrings.onboardingItemSubTitleThree),
+];
